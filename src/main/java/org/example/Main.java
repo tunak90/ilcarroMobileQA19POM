a71be3db73e64d47c7847b97da64115bf3a228cf
tunak90 <dombrovskaya.diana@gmail.com>
2023-08-09 21:40:21 +0300

    upd main.class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -16,6 +16,7 @@ public class Main {
             System.out.println("i = " + i);
             System.out.println("i = " + i);
             System.out.println("i = " + i);
+            System.out.println("i = " + i);
         }
     }
 }
